9f89e46014ae5bde198eb8a81d6a3c865696206c
Saytzeff-D <davidatanda2020@gmail.com>
2022-10-08 14:00:47 +0100

    working on the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className='theme-color'>
+                <div className='d-flex justify-content-between px-5 text-white py-4'>
+                    <p className='h5'>1aidentiti</p>
+                    <button className='btn btn-outline-light'>Get the App</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
