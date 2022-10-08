cdfc0537328a40aec32287a0901303ab4e692cd5
Saytzeff-D <davidatanda2020@gmail.com>
2022-10-08 23:02:05 +0100

    the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import SubHeader from './components/SubHeader';
+import ThirdSection from './components/ThirdSection';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SubHeader />
+      <ThirdSection />
     </div>
   );
 }
