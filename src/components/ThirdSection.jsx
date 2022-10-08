97ce26d1f181a07f4caf6ec0b77d5374e235e3b8
Saytzeff-D <davidatanda2020@gmail.com>
2022-10-08 23:01:49 +0100

    third-section in improvement

---
+++ src/components/ThirdSection.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const ThirdSection = () => {
+    return (
+        <div>
+            <div className='container'>
+                <p className='text-center h1 theme-color'>Our 1aidentiti solution powers multi-currency accounts, FX, payins and payout integration</p>
+                <div className='row w-100 mx-auto my-5'>
+                    <div className='col-md-4'>
+                        <div className='card'>
+                            <p className='card-header h6 text-muted'>
+                                Payins
+                            </p>
+                            <div className='card-body'>
+                                <p className='h6 theme-color'>Seamlessly receive funds from your customers</p>
+                                <p className='text-muted h6'>
+                                    Collect local payments from anywhere, anytime, with one single account through our multi-currency and 1aidentiti solution.We help e-commerce businesses receive payments swiftly and save money on transaction fees.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-4'>
+                        <div className='card'>
+                            <p className='card-header h6 text-muted'>
+                                Payout
+                            </p>
+                            <div className='card-body'>
+                                <p className='h6 theme-color'>Helping financial institutions make local and international payments.</p>
+                                <p className='text-muted h6'>
+                                1aidentiti helps global financial institutions make cheaper, faster and safer local payments to emerging markets - skipping the unnecessary hefty transaction fees, long settlement time and lack of liquidity
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-4'>
+                        <div className='card'>
+                            <p className='card-header h6 text-muted'>
+                                Onboarding
+                            </p>
+                            <div className='card-body'>
+                                <p className='h6 theme-color'>Fast and secure onboarding process</p>
+                                <p className='text-muted h6'>
+                                Fintechs and startups can quickly onboard their retail and business customers with our 1aidentiti solution. By simply connecting their systems to our solution, their customers can now get onboarded and verified automatically
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ThirdSection;
