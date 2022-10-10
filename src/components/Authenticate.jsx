e3d4e4e57bf4d8f4e0d3255cdc78f63aed91d959
Saytzeff-D <davidatanda2020@gmail.com>
2022-10-10 15:09:55 +0100

    moved the logo to the top

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -5,17 +5,16 @@ const Authenticate = () => {
     return (
         <div>
             <div className='row w-100 mx-0 box-position'>
-                <div className='col-6 theme-bg p-5'>
+                <div className='col-6 theme-bg p-5 d-none d-md-block'>
+                    <img alt='logo' src={require('../assets/logo2.png')} width='100px' className='m-5' />
                     <p className='p-5 h3'>
                         <span className='text-dark'>Cheaper, Faster & Safer B2B FX Payments</span> <span className='text-white'>with a business account</span>
                     </p>
-                    {/* <p className='p-5 h4 text-white'>Pay and receive cross-border payment with ease</p> */}
-                    <img alt='logo' src={require('../assets/logo2.png')} width='200px' className='m-5' />
                     <p className='p-5 h3'>
                         <span className='text-white'>Join other businessess who enjoy</span> <span className='text-dark'>cheaper FX rates, faster settlement periods and liquidity</span>
                     </p>
                 </div>
-                <div className='col-6 p-5'>
+                <div className='col-md-6 p-5'>
                     <Outlet />
                 </div>
             </div>
