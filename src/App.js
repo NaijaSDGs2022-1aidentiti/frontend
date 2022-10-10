4a077f5138d35d27a445ab216494f4734d33edd2
Saytzeff-D <davidatanda2020@gmail.com>
2022-10-10 13:46:20 +0100

    nested some routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Authenticate from './components/Authenticate';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Otp from './pages/Otp';
 import Register from './pages/Register';
+import Verify from './pages/Verify';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+        <Route path='/auth' element={<Authenticate />} >
+          <Route path='/auth/login' element={<Login />} />
+          <Route path='/auth/register' element={<Register />} />
+          <Route path='/auth/otp' element={<Otp />} />
+          <Route path='/auth/verify-reg' element={<Verify />} />
+        </Route>
       </Routes>
     </>
   );
