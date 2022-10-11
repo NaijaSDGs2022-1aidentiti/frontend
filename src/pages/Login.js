76c35f0afc488a126ccf196ba421f913d17da842
Saytzeff-D <davidatanda2020@gmail.com>
2022-10-11 14:16:49 +0100

    validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useFormik } from 'formik';
 import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
 
 const Login = () => {
     const navigate = useNavigate()
+    const formik = useFormik({
+        initialValues: {
+            phrase: ''
+        },
+        validationSchema: Yup.object({
+            phrase: Yup.string().required('Enter your phrase')
+        }),
+        onSubmit: (values)=>{
+            console.log(values)
+        }
+    })
     return (
         <div>
             <div className='py-5 my-md-5'>
@@ -10,11 +23,12 @@ const Login = () => {
                     <img alt='logo' src={require('../assets/logo1.png')} width='150px' height='150px' />
                 </div>
                 <p className='h4 py-4 px-md-5'>Login </p>
-                <form className='px-0 px-md-5'>
+                <form className='px-0 px-md-5' onSubmit={formik.handleSubmit}>
                     <div className='form-group'>
-                        <input className='form-control' placeholder='Enter your Phrase' required />
+                        <input className='form-control' placeholder='Enter your Phrase' onChange={formik.handleChange} onBlur={formik.handleBlur} name='phrase' />
+                        {formik.touched.phrase && <div className='text-danger'>{formik.errors.phrase}</div>}
                     </div>
-                    <button className='btn theme-bg text-white font-weight-bold btn-block'>SIGN IN</button>
+                    <button type='submit' className='btn theme-bg text-white font-weight-bold btn-block'>SIGN IN</button>
                     <p className='py-5 h6'>Don't have an account yet? <span className='theme-color cursor-pointer' onClick={()=>navigate('/auth/register')} >Sign up</span></p>
                 </form>
             </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,32 @@
+import { useFormik } from 'formik';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup'
 
 const Register = () => {
     const navigate = useNavigate()
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            phrase: '',
+            confirm_phrase: ''
+        },
+        validate: (values)=>{
+            let errors = {}
+            if(values.phrase !== values.confirm_phrase) {
+                errors.confirm_phrase = 'Phrase not match'
+            }
+            return errors
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().required('This field is required').email('Enter a valid E-mail'),
+            phrase: Yup.string().required('Phrase is required'),
+            confirm_phrase: Yup.string().required('Confirm your Phrase')
+        }),
+        onSubmit: (values)=>{
+            console.log(values)
+        }
+    })
     return (
         <div>
             <div className='px-0 py-5'>
@@ -10,17 +34,20 @@ const Register = () => {
                     <img alt='logo' src={require('../assets/logo1.png')} width='150px' height='150px' />
                 </div>
                 <p className='h4 px-md-5 py-4'>Register</p>
-                <form className='px-0 px-md-5'>
+                <form className='px-0 px-md-5' onSubmit={formik.handleSubmit}>
                     <div className='form-group'>
-                        <input className='form-control' placeholder='Email' />  
+                        <input className='form-control' placeholder='Email' name='email' onChange={formik.handleChange} onBlur={formik.handleBlur} /> 
+                        {formik.touched.email && <div className='text-danger'>{formik.errors.email}</div>} 
                     </div>
                     <div className='form-group'>
-                        <input className='form-control' placeholder='Phrase' />  
+                        <input className='form-control' placeholder='Phrase' name='phrase' onChange={formik.handleChange} onBlur={formik.handleBlur} /> 
+                        {formik.touched.phrase && <div className='text-danger'>{formik.errors.phrase}</div>}  
                     </div>
                     <div className='form-group'>
-                        <input className='form-control' placeholder='Confirm Phrase' required />
+                        <input className='form-control' placeholder='Confirm Phrase' name='confirm_phrase' onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                        {formik.touched.confirm_phrase && <div className='text-danger'>{formik.errors.confirm_phrase}</div>} 
                     </div>
-                    <button className='btn theme-bg text-white font-weight-bold btn-block'>Continue...</button>
+                    <button type='submit' className='btn theme-bg text-white font-weight-bold btn-block'>Continue...</button>
                     <p className='py-2 h6'>Already sign up? <span className='theme-color cursor-pointer' onClick={()=>navigate('/auth/login')} >Sign in</span></p>
                 </form>
             </div>
