722794111208ff4916f1c94e85e5aecc604d7729
Saytzeff-D <davidatanda2020@gmail.com>
2022-10-14 00:15:02 +0100

    modified the navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,21 +6,28 @@ const Nav = () => {
     return (    
         <div>
             <nav className='navbar navbar-inverse navbar-expand-sm bg-white text-dark'>
-                <a href='/' className='navbar-brand text-dark font-weight-bold text-lowercase h3 animate__animated animate__slideInLeft animate__slow px-md-5 px-1 fs-9' >
-                 <img alt='logo' src={require('../assets/logo1.png')} width='30px' className='py-0 my-0' /> <span className='text-capitalize'>CashFlakes</span>
+                <a href='/' className=' navbar-brand text-dark font-weight-bold text-lowercase h3 animate__animated animate__slideInLeft animate__slow px-md-5 px-1 fs-9 pt-3' >
+                 <img alt='logo' src={require('../assets/logo1.png')} width='30px' className='my-0' /> <span className='text-capitalize mt-5'>CashFlakes</span>
                 </a>
-                <div className='collapse navbar-collapse justify-content-end animate__animated animate__fadeInRight animate__slower py-2'>
+                <div className='collapse navbar-collapse justify-content-start animate__animated animate__fadeInRight animate__slower py-0'>
                     <ul className='navbar-nav'>
-                        <li className='nav-item my-1 mx-2'>
+                        <li className='nav-item my-0 mx-2'>
                             <a href='/' className='nav-link text-dark fs-9'>Services</a>
                         </li>
-                        <li className='nav-item my-1 mx-2'>
+                        <li className='nav-item my-0 mx-2'>
                             <a href='/' className='nav-link text-dark fs-9'>Work with us</a>
                         </li>
-                        <li className='nav-item my-1 mx-2'>
+                        <li className='nav-item my-0 mx-2'>
                             <a href='/' className='nav-link text-dark fs-9'>About</a>
                         </li>
-                        <li className='nav-item my-1 mx-2'>
+                    </ul>
+                </div>
+                <div className='collapse navbar-collapse justify-content-end animate__animated animate__fadeInRight animate__slower py-0'>
+                    <ul className='navbar-nav'>
+                        <li className='nav-item my-0 mx-2'>
+                            <i className='fa fa-search pt-3'></i>
+                        </li>
+                        <li className='nav-item my-0 mx-2'>
                             <button className='btn nav-btn rounded-pill text-white font-weight-bold mx-1 px-4' onClick={()=>navigate('/auth/login')} >Sign in</button>
                         </li>
                     </ul>
