11cab63a6149e095c88c4a5b165d4c66ebd06ef0
Saytzeff-D <davidatanda2020@gmail.com>
2022-10-14 00:15:32 +0100

    homepage now refactored

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -3,48 +3,49 @@ import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 const SubHeader = () => {
     return (
-        <div>
-            <div className='container'>
-                <div className='row w-100 mx-auto'>
-                        <div className='col-md-6 py-5'>
-                    <AnimationOnScroll animateIn='animate__fadeIn animate__slower'>
-                            <p className='h1 theme-color my-0'>
-                                One integrated account;
-                            </p>
-                            <p className='h1 theme-color'>
-                                limitless possibilities
-                            </p>
-                            <p className='text-muted small'>
-                            Run your business with everything you need to scale globally. 1aidentiti solution power payments for import & export, e-commerce, marketplaces, SMEs and large corporates.
-                            </p>
-                    </AnimationOnScroll>
+        <div className='px-md-5 px-2 my-3'>
+            <AnimationOnScroll animateIn='animate__fadeIn animate__slower'>
+                <div className='bg-light p-3 rounded-md'>
+                    <div className='row w-100 mx-0'>
+                        <div className='col-md-2 p-1'>
+                            <div className='nav-btn rounded-xl'>
+                                <p className='text-white h3 text-uppercase p-4 text-center font-weight-bold'>How it works</p>
+                                <p className='text-white text-center h6 fs-8 px-3 pb-4'>Mobile banking differs from mobile payments</p>
+                            </div>
                         </div>
-                    <div className='col-md-6 py-5'>
-                        <AnimationOnScroll animateIn='animate__slideInRight' className='animate__slower'>
-                            <ul>
-                                <li>
-                                    <div>
-                                        <p className='h5 font-weight-bold'>Payments</p>
-                                        <p className='text-muted h6'>Flexible and integrated payins & payout processing solution</p>
+                        <div className='col-md-10'>
+                            <div className='row w-100 mx-0 my-1'>
+                                <div className='col-md-6'>
+                                    <div className='d-flex'>
+                                        <img alt='peer' src={require('../assets/crypto_to_bank.png')} width='50px' height='50px' className='rounded-circle mx-1' />
+                                        <p className='text-dark h6 p-3'>Direct Crypto to bank transfer</p>
                                     </div>
-                                </li>
-                                <li>
-                                    <div>
-                                        <p className='h5 font-weight-bold'>Exchange</p>
-                                        <p className='text-muted h6'>Accessible, reliable, real-time FX infrastructure</p>
+                                </div>
+                                <div className='col-md-6'>
+                                    <div className='d-flex'>
+                                        <img alt='peer' src={require('../assets/crypto_to_crypto.png')} width='50px' height='50px' className='rounded-circle mx-1' />
+                                        <p className='text-dark h6 p-3'>Send Crypto to crypto transfers</p>
                                     </div>
-                                </li>
-                                <li>
-                                    <div>
-                                        <p className='h5 font-weight-bold'>Multi-currency wallets</p>
-                                        <p className='text-muted h6'>One account, multiple currencies</p>
+                                </div>
+                            </div>
+                            <div className='row w-100 mx-0 my-4'>
+                                <div className='col-md-6'>
+                                    <div className='d-flex'>
+                                        <img alt='peer' src={require('../assets/crypto_to_wallet.png')} width='50px' height='50px' className='mx-1' />
+                                        <p className='text-dark h6 p-3'>Crypto to mobile wallets payments</p>
                                     </div>
-                                </li>
-                            </ul>
-                        </AnimationOnScroll>
+                                </div>
+                                <div className='col-md-6'>
+                                    <div className='d-flex'>
+                                        <img alt='peer' src={require('../assets/peer.png')} width='50px' height='50px' className='rounded-circle mx-1' />
+                                        <p className='text-dark h6 p-3'>Peer to peer trading</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </AnimationOnScroll>
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Nav from '../components/Nav'
+import SubHeader from '../components/SubHeader';
 
 const Home = () => {
     const navigate =useNavigate()
@@ -10,19 +11,43 @@ const Home = () => {
                 <Nav />
                 <div className='row w-100 mx-0 px-md-5'>
                     <div className='col-md-4'>
-                        <p className='display-2 font-weight-bold pt-4'>The bank that does it all</p>
-                        <p className='cashflakes-muted'>
+                        <p className='display-4 font-weight-bold pt-4 animate__animated animate__backInUp animate__slower'>Now pay for everything with your crypto</p>
+                        <p className='cashflakes-muted animate__animated animate__fadeIn animate__slower'>
                             By installing this application on your phone, you will benefit from excellent financial services
                         </p>
-                        <button onClick={()=>navigate('/auth/register')} className='btn nav-btn text-white font-weight-bold rounded-pill px-4 py-2'>
-                            Get Started
-                        </button>
+                        <div className='d-flex animate__animated animate__backInDown animate__slower flex-md-row flex-column'>
+                            <button onClick={()=>navigate('/auth/register')} className='btn nav-btn text-white font-weight-bold rounded-pill px-4 py-2 mx-1 my-1'>
+                                Get Started
+                            </button>
+                            <button className='btn text-dark font-weight-bold rounded-pill px-4 py-2 mx-1 my-1'>
+                               <span className='bg-white px-2 py-1 rounded-circle shadow-lg'><i className='fa fa-apple'></i></span> Download for IOS
+                            </button>
+                        </div>
                     </div>
-                    <div className='col-md-8'>
+                    <div className='col-md-8 animate__animated animate__fadeIn animate__slower d-none d-md-block'>
+                        <div className='bg-white shadow-lg rounded-pill mini-text px-2 py-1'>
+                            <div className='d-flex'>
+                                <img alt='miniImage' src={require('../assets/transfer.png')} width='50px' height='50px' className='mt-2 bg-white shadow-lg rounded-circle mr-2' />
+                                <div className='px-4'>
+                                    <p className='font-weight-bold text-dark fs-8 my-0 pt-2'>David Ololade</p>
+                                    <p className='fs-8 cashflakes-muted'>08164572165........</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='bg-white shadow-lg rounded-pill max-text px-2 py-1'>
+                            <div className='d-flex'>
+                                <img alt='miniImage' src={require('../assets/secure.png')} width='50px' height='50px' className='mt-2 bg-white shadow-lg rounded-circle mr-2' />
+                                <div className='px-4'>
+                                    <p className='font-weight-bold text-dark my-0 pt-2 fs-8'>Secured Payment</p>
+                                    <p className='fs-8 cashflakes-muted'>Anywhere...</p>
+                                </div>
+                            </div>
+                        </div>
                         <img alt='bg-logo' src={require('../assets/two.png')} className='w-100'  />
                     </div>
                 </div>
             </div>
+            <SubHeader />
         </div>
     );
 };
